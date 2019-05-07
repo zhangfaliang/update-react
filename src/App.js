aa7c896b2cc845b968cdda04bb3d8f5b71ce000c
张发亮 <fl.zhang@8win.com>
2019-05-07 11:55:19 +0800

    add  test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Helmet } from 'react-helmet';
+
 import './App.css';
 import Button from './components/Button';
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My Titlewerwe</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
       <header className="App-header">
         <Button />
       </header>
+      fa-times-rectangle
     </div>
   );
 }
